163ca63680642e8ca30080bbeb53977676d5c3ab
1 <1@DESKTOP-DKNS8PH>
2021-12-22 13:08:23 +0530

    First time commitment

---
+++ GitProject/Array_Char.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Array_Char {
+
+	public static void main(String[] args)
+	{
+		char grade[]=new char[4];
+		grade [0]='A';
+		grade [1]='B';
+		grade [2]='C';
+		grade [3]='D';
+		
+		for (int i=0;i<=grade.length-1;i++)
+		{
+			System.out.println(grade[i]);
+		}
+		
+		
+		
+
+	}
+
+}
